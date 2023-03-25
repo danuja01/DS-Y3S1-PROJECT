1016c9ea94fa642a6a4b03dbbbdd0a3f6d1c8e19
danuja01 <it21023378@my.sliit.lk>
2023-03-25 16:39:40 +0530

    Initialize mono repo structure and common codes for microservices

---
+++ packages/mongoose/src/index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { moduleLogger } from "@sliit-foss/module-logger";
+
+const logger = moduleLogger("Mongoose-DB-Connector");
+
+export const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      keepAlive: true,
+      socketTimeoutMS: 30000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    logger.info("Connected to the database");
+  } catch (error) {
+    logger.error(`Database connection Failed | message: ${error.message}`);
+  }
+
+  mongoose.connection.on("error", (err) => {
+    logger.error(`Database connection Error | message: ${err.message}`);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    logger.error("Database disconnected");
+  });
+
+  mongoose.connection.on("connected", () => {
+    logger.info("Database connected");
+  });
+};
---
+++ packages/server/src/index.js
@@ -0,0 +1,88 @@
+import fs from "fs";
+import crypto from "crypto";
+import express from "express";
+import expressHealth from "express-health-middleware";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import context from "express-http-context";
+import stack from "callsite";
+import clusterize from "@sliit-foss/clusterizer";
+import { moduleLogger } from "@sliit-foss/module-logger";
+import { correlationId } from "@app/constants";
+import { errorHandler, responseInterceptor } from "@app/middleware";
+import { connect as connectDatabase } from "@app/mongoose";
+
+const initialize = ({
+  service,
+  routes,
+  lMiddleware = [],
+  cors: isCorsEnable,
+  database,
+  config,
+}) => {
+  const logger = moduleLogger("Server");
+  clusterize(
+    () => {
+      const app = express();
+
+      app.use(helmet());
+      app.use(compression());
+
+      app.use(express.json({ limit: "1mb" }));
+      app.use(express.urlencoded({ extended: true }));
+
+      if (isCorsEnable) {
+        app.use(cors());
+      }
+
+      app.use(context.middleware);
+
+      app.use((req, _res, next) => {
+        context.set(
+          "correlationId",
+          req.headers[correlationId] ?? crypto.randomBytes(16).toString("hex")
+        );
+        next();
+      });
+
+      if (database) {
+        connectDatabase(database);
+      }
+
+      app.use("/system", expressHealth());
+
+      if (!routes) {
+        routes = express.Router();
+        const root = stack()
+          .find((site) => site.getFileName().endsWith("server.js"))
+          ?.getFileName()
+          ?.replace("/server.js", "")
+          ?.replace("\\server.js", "");
+        fs.readdirSync(`${root}/modules`)?.forEach((module) => {
+          fs.readdirSync(`${root}/modules/${module}/api`)?.forEach((v) => {
+            routes.use(
+              `/${v}/${module}`,
+              require(`${root}/modules/${module}/api/${v}/controller`).default
+            );
+          });
+        });
+      }
+
+      app.use(`/api`, ...lMiddleware, routes);
+
+      app.use(responseInterceptor);
+
+      app.use(errorHandler);
+
+      const HOST = config.HOST ?? "0.0.0.0";
+
+      app.listen(config.PORT, HOST, () => {
+        logger.info(`${service} listening on ${HOST}:${config.PORT}`);
+      });
+    },
+    { logger, workers: 1 }
+  );
+};
+
+export default initialize;
