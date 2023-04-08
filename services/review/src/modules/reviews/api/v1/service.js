1f2913838598dbaa78ce1dd6cae038818c86abb1
Menusha Eshan <it21113772@my.sliit.lk>
2023-04-08 19:03:18 +0530

    Review service added

---
+++ services/review/src/modules/reviews/api/v1/service.js
@@ -0,0 +1,32 @@
+import {
+  createReview,
+  getAllReviews,
+  getReviewById,
+  updateReviewById,
+  deleteReviewById,
+} from "../../repository";
+
+import { hashPasswordIfProvided } from "./helpers";
+
+export const serviceCreateReview = async (review) => {
+  if (!review.rating) throw new Error("Rating is required");
+  await hashPasswordIfProvided(review);
+  return createReview(review);
+};
+
+export const serviceGetReviews = (filters, sorts, page, limit) => {
+  return getAllReviews({ filters, sorts, page, limit });
+};
+
+export const serviceGetReviewById = (id) => {
+  return getReviewById(id);
+};
+
+export const serviceUpdateReviewById = async (id, data) => {
+  await hashPasswordIfProvided(data);
+  return updateReviewById(id, data);
+};
+
+export const serviceDeleteReviewById = (id) => {
+  return deleteReviewById(id);
+};
