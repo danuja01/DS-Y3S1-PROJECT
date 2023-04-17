15030cec62a5032e7f0292f6344f0383f3a88d87
danuja01 <it21023378@my.sliit.lk>
2023-04-17 21:58:12 +0530

    created mail service

---
+++ services/email/src/services/nodemailer.service.js
@@ -0,0 +1,40 @@
+import nodemailer from "nodemailer";
+import handlebars from "handlebars";
+import config from "../config";
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: config.MAIL_USER,
+    pass: config.MAIL_PASSWORD,
+  },
+  pool: true,
+});
+
+export const sendMail = ({
+  to,
+  cc = [],
+  bcc = [],
+  templateHTML,
+  replacements,
+  subject,
+  attachments = [],
+}) => {
+  const template = handlebars.compile(templateHTML);
+  const htmlToSend = template(replacements);
+  const mailOptions = {
+    from: config.MAIL_USER,
+    to,
+    cc,
+    bcc,
+    subject: subject,
+    html: htmlToSend,
+    attachments,
+  };
+  return new Promise((resolve, reject) => {
+    transport.sendMail(mailOptions, (error) => {
+      if (error) reject(error);
+      resolve(true);
+    });
+  });
+};
