2235fbe24ecdc491d400e0d9a31c85211d9ba3bf
Yohan Perera <it21131424@my.sliit.lk>
2023-04-17 11:04:48 +0530

    randomFrontendfixing

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4006/api/v1/notification",
+  baseURL: "http://localhost:4006/api/v1/notifications",
   headers: {
     "Content-type": "application/json",
   },
